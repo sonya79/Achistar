2e9a40ad6fde49acc143a98b971e8e6c5893c61c
Sonya <eldarova.sonya@gmail.com>
2023-11-26 16:31:44 -0500

    Add Dockerfile and Hello.java

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM openjdk:8
+COPY . /var/www/java
+WORKDIR /var/www/java
+RUN javac Hello.java
+CMD ["java", "Hello"]
+
+
---
+++ Hello.java
@@ -0,0 +1,7 @@
+nano Hello.java
+  class Hello {
+    public static void main(String[] args) {
+      System.out.println("This is a java app created using Docker");
+    }
+  }
+
